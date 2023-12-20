9427fb13073ec40cd7da74b64822ad25b31c3f60
kynan justis <kfjustis@gmail.com>
2023-12-20 11:11:13 -0600

    Solve day-01 part1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,3 +1,36 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
 fn main() {
-    println!("this is part1");
+    let file = match File::open("./input.txt") {
+        Err(_) => panic!("bad file"),
+        Ok(file) => file,
+    };
+
+    let mut output: Vec<String> = Vec::new();
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        let line_str = line.unwrap();
+        let mut val_array: Vec<char> = Vec::new();
+        for c in line_str.chars() {
+            if c.is_digit(10) {
+                val_array.push(c);
+                break;
+            }
+        }
+        for c in line_str.chars().rev() {
+            if c.is_digit(10) {
+                val_array.push(c);
+                break;
+            }
+        }
+        output.push(val_array.iter().collect());
+    }
+
+    let mut sum: u32 = 0;
+    for s in output.iter_mut() {
+        sum += s.parse::<u32>().unwrap();
+    }
+
+    println!("final output: {}", sum);
 }
